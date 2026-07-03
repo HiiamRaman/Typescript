f9242e3a758162d345cc2c37d2de912a9e8b3420
Raman Singh <ramansinghchauhan787@gmail.com>
2026-07-03 09:39:03 +0545

    interfaces

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Raman");
+const dog = {
+    name: "Jack",
+    sound: 'meow'
+};
+console.log(dog.sound);
+const user = {
+    name: "kale",
+    age: 23,
+    role: "doctor"
+};
+console.log(user.name, user.age);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("Raman")
+interface Animal {
+    name:string
+}
+
+interface Dog extends Animal {
+
+    sound:string
+
+} 
+
+
+const  dog:Dog = {
+    name:"Jack",
+    sound:'meow'
+
+}
+console.log(dog.sound)
+
+
+
+
+
+interface User {
+    name:string;
+    age?:number
+}
+
+interface Admin extends User {
+    role:string
+}
+
+
+const user:Admin = {
+name:"kale",
+age:23,
+role:"doctor"
+}
+
+console.log(user.name,user.age)
+
+
